1a9d6c375580dd8c502005057634d1e94572537f
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2025-03-08 12:11:54 +0100

    up deps

--- scaleway.go
+++ scaleway.go
@@ -3,7 +3,7 @@ package scaleway
 import (
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
-	"github.com/libdns/scaleway"
+	"github.com/okami101/libdns-scaleway"
 )
 
 // Provider lets Caddy read and manipulate DNS records hosted by this DNS provider.
